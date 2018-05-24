e52064486e986946035d0a1469dd72c2a37fd769
winslow <winslowwinslow@me.com>
2018-05-24 10:49:45 -0400

    Add files via upload

---
+++ resources/tpl/viewer/nls/lv/template.js
@@ -0,0 +1,65 @@
+define({
+  "viewer": {
+    "common": {
+      "close": "Aizvērt"
+    },
+    "loading": {
+      "long": "Lietotne tiek inicializēta",
+      "long2": "Paldies, ka uzgaidījāt",
+      "failButton": "Pārlādēt lietotni"
+    },
+    "signin": {
+      "title": "Nepieciešama autentifikācija",
+      "explainViewer": "Lai piekļūtu lietotnei, pierakstieties %PORTAL_LINK%, izmantojot kontu.",
+      "explainBuilder": "Lai konfigurētu lietotni, pierakstieties %PORTAL_LINK%, izmantojot kontu."
+    },
+    "errors": {
+      "boxTitle": "Kļūda",
+      "invalidConfig": "Nederīga konfigurācija",
+      "invalidConfigNoApp": "Tīmekļa kartēšanas lietotnes identifikators nav norādīts šeit: index.html.",
+      "invalidConfigNoAppDev": "URL parametros nav norādīts tīmekļa kartēšanas lietotnes identifikators (?appid=). Izstrādes režīmā lapā index.html ir ignorēta appid konfigurācija.",
+      "unspecifiedConfigOwner": "Pilnvarotais īpašnieks nav konfigurēts.",
+      "invalidConfigOwner": "Stāsta īpašnieks nav pilnvarots.",
+      "createMap": "Nevar izveidot karti",
+      "invalidApp": "%TPL_NAME% nepastāv vai tai nevar piekļūt.",
+      "appLoadingFail": "Radusies kāda kļūda: nepareizi ielādējās %TPL_NAME%.",
+      "notConfiguredDesktop": "Stāsts vēl nav konfigurēts.",
+      "notConfiguredMobile": "%TPL_NAME% veidotājs nav atbalstīts šim displeja izmēram. Ja iespējams, mainiet pārlūka izmērus tā, lai piekļūtu veidotājam, vai veidojiet savu stāstu ierīcē ar lielāku ekrānu.",
+      "notConfiguredMobile2": "Lai izmantotu %TPL_NAME% veidotāju, rotējiet ierīci uz ainavas orientāciju.",
+      "notAuthorized": "Jūs neesat pilnvarots piekļūt šim stāstam",
+      "notAuthorizedBuilder": "Jums neesat pilnvarots izmantot %TPL_NAME% veidotāju.",
+      "noBuilderIE": "Veidotājs netiek atbalstīts Internet Explorer versijās, kas ir vecākas par %VERSION%. %UPGRADE%",
+      "noViewerIE": "Šis stāsts netiek atbalstīts Internet Explorer versijās, kas ir vecākas par %VERSION%. %UPGRADE%",
+      "noViewerIE2": "Jūs mēģināt skatīt šo stāstu, izmantojot vecāku, neatbalstītu pārlūku. Var gadīties, ka nedarbojas kāds elements vai rodas citas neparedzētas problēmas. Iesakām jaunināt uz Internet Explorer 11 vai izmantot citu pārlūku, piemēram, Chrome.",
+      "noViewerIE3": "No 2017. gada beigām šo stāstu vairs nevarēs ielādēt šajā pārlūkā. Pēc tam šī stāsta skatīšanai ir jāizmanto atbalstīts pārlūks.",
+      "upgradeBrowser": "<a href='http://browsehappy.com/' target='_blank'>Atjaunojiet savu pārlūku</a>.",
+      "mapLoadingFail": "Radās kļūda — karte netika ielādēta pareizi.",
+      "signOut": "Izrakstīties",
+      "attention": "Uzmanību!",
+      "noBuilderEdge": "Lietotnes Story Map Shortlist sarakstus var skatīt jebkurā Web pārlūkā. Tomēr funkcija Story Map Shortlist Builder nav pieejama pārlūkā Windows Edge. Izvēles sarakstu veidošanai vai rediģēšanai izmantojiet citu Web pārlūku.",
+      "noBuilderAllIE": "Lietotnes Story Map Shortlist sarakstus var skatīt jebkurā tīmekļa pārlūkā. Tomēr funkcija Story Map Shortlist Builder nav pieejama pārlūkā Microsoft Edge vai Internet Explorer. Shortlist sarakstu veidošanai vai labošanai izmantojiet citu tīmekļa pārlūku."
+    },
+    "headerFromCommon": {
+      "storymapsText": "Kartes stāsts",
+      "builderButton": "Labot",
+      "facebookTooltip": "Kopīgot Facebook",
+      "twitterTooltip": "Kopīgot Twitter",
+      "bitlyTooltip": "Saņemt īsu saiti uz lietotni",
+      "templateTitle": "Iestatīt sagataves virsrakstu",
+      "templateSubtitle": "Iestatīt sagataves apakšvirsrakstu",
+      "share": "Kopīgot"
+    },
+    "shareFromCommon": {
+      "copy": "Kopēt",
+      "copied": "Nokopēts",
+      "open": "Atvērt",
+      "embed": "Iedarināt tīmekļa lapā",
+      "embedExplain": "Izmantojiet šo HTML kodu, lai lietotni iedarinātu tīmekļa lapā.",
+      "size": "Izmēri (platums/augstums):"
+    },
+    "general": {
+      "noPlaces": "Šajā teritorijā vietu nav. Lai redzētu vietas, attāliniet vai klikšķiniet uz sākuma pogas.",
+      "start": "Sākt"
+    }
+  }
+});
---
+++ resources/tpl/viewer/nls/pl/template.js
@@ -0,0 +1,65 @@
+define({
+  "viewer": {
+    "common": {
+      "close": "Zamknij"
+    },
+    "loading": {
+      "long": "Inicjalizacja aplikacji",
+      "long2": "Dziękujemy za cierpliwość",
+      "failButton": "Ponownie wczytaj aplikację"
+    },
+    "signin": {
+      "title": "Wymagane jest uwierzytelnienie",
+      "explainViewer": "Aby uzyskać dostęp do aplikacji, zaloguj się na konto w portalu %PORTAL_LINK%.",
+      "explainBuilder": "Aby skonfigurować aplikację, zaloguj się na konto w portalu %PORTAL_LINK%."
+    },
+    "errors": {
+      "boxTitle": "Wystąpił błąd",
+      "invalidConfig": "Nieprawidłowa konfiguracja",
+      "invalidConfigNoApp": "Identyfikator aplikacji tworzenia map internetowych nie został określony w pliku index.html.",
+      "invalidConfigNoAppDev": "W parametrach adresu URL nie określono identyfikatora aplikacji map internetowych (?appid=). W trybie deweloperskim konfiguracja parametru appid w pliku index.html jest ignorowana.",
+      "unspecifiedConfigOwner": "Nie skonfigurowano autoryzowanego właściciela.",
+      "invalidConfigOwner": "Właściciel narracji nie jest autoryzowany.",
+      "createMap": "Nie można utworzyć mapy",
+      "invalidApp": "%TPL_NAME% nie istnieje lub jest niedostępna.",
+      "appLoadingFail": "Wystąpił pewien problem, nie wczytano prawidłowo %TPL_NAME%.",
+      "notConfiguredDesktop": "Narracja nie została jeszcze skonfigurowana.",
+      "notConfiguredMobile": "Kreator %TPL_NAME% nie jest obsługiwany dla wyświetlacza w tym rozmiarze. Jeśli to możliwe, zmień rozmiar okna przeglądarki, aby uzyskać dostęp do kreatora, lub buduj narrację na urządzeniu z większym ekranem.",
+      "notConfiguredMobile2": "Aby korzystać z kreatora %TPL_NAME%, obróć urządzenie do orientacji poziomej.",
+      "notAuthorized": "Nie masz uprawnień dostępu do tej narracji",
+      "notAuthorizedBuilder": "Nie masz uprawnień do korzystania z kreatora %TPL_NAME%.",
+      "noBuilderIE": "Kreator nie jest obsługiwany przez przeglądarkę Internet Explorer w wersji starszej niż %VERSION%. %UPGRADE%",
+      "noViewerIE": "Ta narracja nie jest obsługiwana przez przeglądarkę Internet Explorer w wersji starszej niż %VERSION%. %UPGRADE%",
+      "noViewerIE2": "Próbujesz wyświetlić tę narrację za pomocą starszej, nieobsługiwanej przeglądarki. Niektóre funkcje mogą nie działać lub mogą wystąpić inne nieoczekiwane problemy. Zalecamy zaktualizowanie przeglądarki do programu Internet Explorer 11 lub użycie innej przeglądarki, na przykład Chrome.",
+      "noViewerIE3": "Pod koniec 2017 roku ta narracja nie będzie się już wczytywała w tej przeglądarce. Do jej wyświetlenia konieczne będzie użycie obsługiwanej przeglądarki.",
+      "upgradeBrowser": "<a href='http://browsehappy.com/' target='_blank'>Prosimy o zaktualizowanie przeglądarki</a>.",
+      "mapLoadingFail": "Wystąpił pewien problem, nie wczytano mapy prawidłowo.",
+      "signOut": "Wyloguj się",
+      "attention": "Uwaga!",
+      "noBuilderEdge": "Krótkie listy aplikacji Story Map — Krótka lista można wyświetlać w każdej przeglądarce internetowej. Należy jednak pamiętać, że kreator tej aplikacji nie jest obsługiwany w przeglądarce Windows Edge. Do tworzenia i edycji krótkich list należy używać innych przeglądarek.",
+      "noBuilderAllIE": "Story Map — Krótka lista to szablon, który można wyświetlać w każdej przeglądarce internetowej. Należy jednak pamiętać, że kreator tej aplikacji nie jest obsługiwany w przeglądarkach Microsoft Edge i Internet Explorer. Do tworzenia i edycji krótkich list należy używać innych przeglądarek."
+    },
+    "headerFromCommon": {
+      "storymapsText": "Mapa narracyjna",
+      "builderButton": "Edytuj",
+      "facebookTooltip": "Udostępnij na Facebooku",
+      "twitterTooltip": "Udostępnij na Twitterze",
+      "bitlyTooltip": "Pobierz skrócone łącze do aplikacji",
+      "templateTitle": "Skonfiguruj tytuł szablonu",
+      "templateSubtitle": "Skonfiguruj podtytuł szablonu",
+      "share": "Udostępnianie"
+    },
+    "shareFromCommon": {
+      "copy": "Kopiuj",
+      "copied": "Skopiowane",
+      "open": "Otwórz",
+      "embed": "Osadzono na stronie internetowej",
+      "embedExplain": "Aby umieścić Dziennik na stronie internetowej, skorzystaj z poniższego kodu HTML.",
+      "size": "Rozmiar (szerokość/wysokość):"
+    },
+    "general": {
+      "noPlaces": "W tym obszarze nie ma żadnych miejsc. Pomniejsz lub kliknij przycisk Start, aby wyświetlić miejsca.",
+      "start": "Uruchom"
+    }
+  }
+});
